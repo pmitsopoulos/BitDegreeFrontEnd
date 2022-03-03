20c19bc8bded6fd349dc7a74878cab97af6577b1
Panagiotis Mitsopoulos <panagiotis.mitsopoulos@outlook.com>
2022-03-03 18:21:02 +0200

    Complete Assignmetn 8.4 Sprint 14

---
+++ Sprint_14/Assignment-8_4/webpack.config.js
@@ -0,0 +1,60 @@
+// Webpack uses this to work with directories
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+// This is the main configuration object.
+// Here, you write different options and tell Webpack what to do
+module.exports = {
+  // Path to your entry point. From this file Webpack will begin its work
+  entry: "./src/index.js",
+
+  // Path and filename of your result bundle.
+  // Webpack will bundle all JavaScript into this file
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "",
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader', 
+        exclude: /node_modules/ 
+      },
+      // Now we apply a rule for images
+      {
+        test: /\.(png|jpe?g|gif|svg)$/,
+        use: [
+               {
+                 //By using file-loader for these files.
+                 loader: "file-loader",
+               }
+           ]
+       },
+       {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          {
+            // It gets all of the transformed CSS and extracts it into a separate
+            // single bundled file.
+            loader: MiniCssExtractPlugin.loader
+          }, 
+          {
+            loader: "css-loader",
+          },
+        ]
+      },
+    ]       
+  },
+  plugins: [ 
+    new MiniCssExtractPlugin({
+      filename:"bundle.css"
+    })
+  ],
+  // Default mode for Webpack is production.
+  // Depending on mode Webpack will apply different things
+  // on the final bundle. For now, we don't need production's JavaScript
+  // minifying and other things, so let's set mode to development
+  mode: "development",
+};
