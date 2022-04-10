2a1feb5eef48b0b69e3f42839edcb751cb729dff
Panagiotis Mitsopoulos <pmdv147@gmail.com>
2022-04-10 23:51:03 +0300

    Complete Sprint 19 Project

--- Sprint_19/Project/tic-tac-toe/src/app/game-logic.ts
+++ Sprint_19/Project/tic-tac-toe/src/app/game-logic.ts
@@ -1,18 +1,120 @@
-import {Status} from "./game-status";
-
+import {
+  Status
+} from "./game-status";
 export class GameLogic {
-    gameStatus!: Status;
-    gameField: Array<number> = [];
-    currentTurn!: number;
+  gameStatus!: Status;
+  gameField: Array < number > = [];
+  currentTurn!: number;
+  // game!: Object
+  winConditionsOne: Array < Array < number >> = [
+    [1, 1, 1, 0, 0, 0, 0, 0, 0], // Horizontal wins
+    [0, 0, 0, 1, 1, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 1, 1, 1],
+    [1, 0, 0, 1, 0, 0, 1, 0, 0], // Vertical wins
+    [0, 1, 0, 0, 1, 0, 0, 1, 0],
+    [0, 0, 1, 0, 0, 1, 0, 0, 1],
+    [1, 0, 0, 0, 1, 0, 0, 0, 1], // Diagonal wins
+    [0, 0, 1, 0, 1, 0, 1, 0, 0],
+  ]
+
+  winConditionsTwo: Array < Array < number >> = [
+    [2, 2, 2, 0, 0, 0, 0, 0, 0], // Horizontal wins
+    [0, 0, 0, 2, 2, 2, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 2, 2, 2],
+    [2, 0, 0, 2, 0, 0, 2, 0, 0], // Vertical wins
+    [0, 2, 0, 0, 2, 0, 0, 2, 0],
+    [0, 0, 2, 0, 0, 2, 0, 0, 2],
+    [2, 0, 0, 0, 2, 0, 0, 0, 2], // Diagonal wins
+    [0, 0, 2, 0, 2, 0, 2, 0, 0],
+  ]
 
 
-    public constructor(){
-        this.gameField = [0,0,0,0,0,0,0,0,0];
-        this.gameStatus = Status.STOP;
-        this.currentTurn = 1;
+  public constructor() {
+    this.gameField = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+    this.gameStatus = Status.STOP;
+    this.currentTurn = 1;
+  }
+
+  gameStart(): void {
+    this.gameStatus = Status.START;
+    this.gameField = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+    this.currentTurn = this.randomPlayerStart();
+    console.log(this.currentTurn);
+  }
+
+  randomPlayerStart(): number {
+
+    const startPlayer = Math.floor(Math.random() * 2) + 1;
+    return startPlayer;
+  }
+
+  setField(positon: number, value: number): void {
+    this.gameField[positon] = value;
+    console.log(this.gameField);
+  }
+
+  getPlayerColorClass(): string {
+    let colorClass = this.currentTurn === 1 ? "player-one" : "player-two";
+    return colorClass;
+  }
+
+  changePlayer(): void {
+    this.currentTurn = this.currentTurn === 1 ? 2 : 1;
+  }
+
+  async checkGameEndFull(): Promise < boolean > {
+    let isFull = true;
+
+    if (this.gameField.includes(0)) {
+      console.log("empty spaces still exist")
+      isFull = false;
     }
-
-    startGame(): void{
-        this.game.gameStart();
+    if (isFull) {
+      this.gameEnd();
+      return true;
+    } else {
+      return false;
     }
+  }
+
+  arrayEquals(a: Array<any>, b: Array<any>): boolean {
+    return Array.isArray(a) && Array.isArray(b) && a.length === b.length && a.every((value, index) => value === b[index])
+  }
+
+  async checkGameEndWinner(): Promise < boolean > {
+    let isWinner = false;
+
+    const checkWin = (this.currentTurn === 1) ? this.winConditionsOne : this.winConditionsTwo;
+    const currentBoard: any = [];
+
+    this.gameField.forEach((subfield, index) => {
+      if (subfield !== this.currentTurn) {
+        currentBoard[index] = 0;
+      } else {
+        currentBoard[index] = subfield;
+      }
+    });
+
+    console.log(`Here's the current state of the board: ${currentBoard}`);
+
+    checkWin.forEach((checkfield, checkindex) => {
+      if (this.arrayEquals(checkfield, currentBoard)) {
+        isWinner = true;
+      }
+    });
+
+    if (isWinner) {
+      console.log('Three in a row: TIC-TAC-TOE!!!')
+      this.gameEnd();
+      console.log("EndGame");
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+
+  gameEnd(): void {
+    this.gameStatus = Status.STOP;
+  }
 }
--- Sprint_19/Project/tic-tac-toe/src/app/game/game.component.ts
+++ Sprint_19/Project/tic-tac-toe/src/app/game/game.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { GameLogic } from '../game-logic';
 
+
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
@@ -9,7 +10,63 @@ import { GameLogic } from '../game-logic';
 })
 export class GameComponent implements OnInit {
 
-  constructor() { }
+  constructor(public game : GameLogic) { }
+  
+  startGame(): void {
+    this.game.gameStart();
+    this.updateCurrentPlayer();
+}
+
+updateCurrentPlayer() : void
+{
+  const currentPlayer = `Current turn: Player ${this.game.currentTurn}`;
+  const information = document.querySelector("#current-status");
+  information!.innerHTML = currentPlayer;
+}
+
+async clickSubfield(subfield: any): Promise<void> {
+  if(this.game.gameStatus === 1)
+  {
+    const position = subfield.currentTarget.getAttribute("position");
+    const whoClicked = {player: this.game.currentTurn, position: position}
+    console.log(whoClicked);
+
+    this.game.setField(position , this.game.currentTurn);
+
+    const color = this.game.getPlayerColorClass();
+
+    subfield.currentTarget.classList.add(color);
+
+    this.game.changePlayer();
+
+    await this.game.checkGameEndWinner().then((end: boolean) => {
+      if (this.game.gameStatus === 0 && end){
+        const information = document.querySelector('#current-status')
+        information!.innerHTML = `The winner is Player ` + this.game.currentTurn;
+      }
+    });
+
+    await this.game.checkGameEndFull().then((end: boolean)=>{
+      if(this.game.gameStatus === 0 && end)
+      {
+        const information = document.querySelector("#current-status");
+        information!.innerHTML = "No winner: it's a draw";
+      }
+    });
+
+    if(this.game.gameStatus === 1)
+    {
+      this.updateCurrentPlayer();
+    }
+  }
+
+
+
+
+}
+
+
+
 
   ngOnInit(): void {
   }
